b5231357158017d53cc659dbf3b8079073aca157
pavan27v <109024676+pavan27v@users.noreply.github.com>
2024-04-13 04:30:08 +0000

    calc.c

---
+++ write_large_program/multi_file_compilation1/stack.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void make_empty()
+{
+    
+    printf("in make_empty");
+}
+
---
+++ write_large_program/multi_file_compilation1/stack.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void make_empty(void);
+int is_empty(void);
+int is_full(void);
+void push(int i);
+int pop(void);
