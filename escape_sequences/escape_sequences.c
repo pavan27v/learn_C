cc748ae91f15fd67ab11f38b5969f7d727fe04bd
pavan27v <109024676+pavan27v@users.noreply.github.com>
2024-04-17 03:46:13 +0000

    escape_sequences.c

---
+++ escape_sequences/escape_sequences.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Escape sequences
+\'  single Quote
+\"  double Quote
+\\  backslash
+\0  NULL
+\a  bell
+\b  backspace
+\f form feed
+\n new line
+\r carry return
+\t horizontal tab
+\v vertical tab
+
+
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+    
+     printf("hello\vworld rjirgigj\f");
+     printf("jjfri\f");
+      printf("jjfri\f");
+     
+   
+
+    return 0;
+}
+
